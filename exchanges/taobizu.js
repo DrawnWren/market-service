81b1bb68f595a106156fc751155df18e30e647e0
Wren <drawnwren@gmail.com>
2016-12-13 13:50:58 -0800

    Added taobizu and an ansible directive for refreshing the repository and restarting the service.

---
+++ exchanges/taobizu.js
@@ -0,0 +1,22 @@
+const get = require('../util/rest').get;
+
+const baseURI = 'http://taobizu.com/trade/chart_json/market/';
+
+function taobizu(pair, time, callback) {
+  let responses = [];
+  setInterval(() => {
+  const uri = `${baseURI}${pair}`;
+  return get(uri)
+    .then( (response) => {
+      const proTime = new Date().getTime();
+      responses.push({ proTime, response });
+    })
+    .catch(e => console.error(e));
+  }, 1000);
+  setInterval(() => {
+    callback({ responses });
+    responses = [];
+  }, time);
+}
+
+module.exports = taobizu;
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const bitmex = require('./exchanges/bitmex');
 const bitfinex = require('./exchanges/bitfinex');
 const poloniex = require('./exchanges/poloniex');
 const gdax = require('./exchanges/gdax');
+const taobizu = require('./exchanges/taobizu');
 
 const upload = require('./util/upload');
 
@@ -32,4 +33,18 @@ gdax(
   pair.gdax,
   time,
   results => upload(results, pair.gdax, 'gdax')
+);
+
+const cny = {
+  bitmex: 'XBC',
+  taobizu: 'BTC_CNY',
+};
+
+bitmex(cny.bitmex, time, results => upload(results, cny.bitmex, 'bitmex'));
+
+
+taobizu(
+  cny.taobizu, 
+  time,
+  results => upload(results, cny.taobizu, 'taobizu')
 );
